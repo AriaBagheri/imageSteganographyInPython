f08d7fc3bbc8bd1619093b5f231ceaa1c2d10e7f
Aria Bagheri <AriaB9342@gmail.com>
2022-10-30 02:09:46 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -10,6 +10,6 @@ secret_data.extend([0] * (len(raw_pixels) - len(secret_data)))
 
 processed_pixels = []
 for i, pixel in enumerate(raw_pixels):
-    processed_pixels.append(((pixel[0] >> 2 << 2) + secret_data[i], pixel[1], pixel[2]))
+    processed_pixels.append(((pixel[0] >> 1 << 1) + secret_data[i], pixel[1], pixel[2]))
 
 Image.fromarray(np.array(processed_pixels, dtype=np.uint8).reshape(raw_image.size[1], raw_pixels.size[0], 3)).save("processed.png")
